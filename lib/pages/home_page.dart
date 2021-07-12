3c6a4024d0a70aad87982e472d04cdd7f25c26e5
mohosin513025 <36011293+mohosin513025@users.noreply.github.com>
2021-07-12 14:18:36 +0530

    day 13 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,15 +1,37 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:project_01/models/catalog.dart';
 import 'package:project_01/widgets/drawer.dart';
 import 'package:project_01/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   // const HomePage({ Key? key }) : super(key: key);
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final int project = 01;
+  final String name = "mohosin akhan";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    print(productsData);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final int project = 01;
-    final String name = "mohosin akhan";
     final dummyList = List.generate(6, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
